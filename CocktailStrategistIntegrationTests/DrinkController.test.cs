ab6f49498da021c768647d8d5f837c32ef916eb9
Jake Atkinson <jake.atkinson@hotmail.com>
2024-10-05 02:19:58 +1000

    fix test projects for restore

--- CocktailStrategistIntegrationTests/DrinkController.test.cs
+++ CocktailStrategistIntegrationTests/DrinkController.test.cs
@@ -1,13 +1,27 @@
+using CocktailStrategist.Data;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using System.Text;
+using System.Transactions;
 
 namespace CocktailStrategistIntegrationTests
 {
-    public class Tests
+    [TestFixture, Rollback]
+    public class DrinkControllerTests
     {
+        private TransactionScope scope;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            scope = new TransactionScope();
+            Console.WriteLine("starting scope");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scope.Dispose();
         }
 
         [Test]
@@ -18,7 +32,7 @@ namespace CocktailStrategistIntegrationTests
             {
             });
             var client = factory.CreateClient();
-            var url = "/api/drink";
+            var url = "/drink";
 
             // Act
 
@@ -27,9 +41,35 @@ namespace CocktailStrategistIntegrationTests
 
             // Assert
             response.EnsureSuccessStatusCode();
-            Console.WriteLine(content);
             Assert.That(content.Equals("value2"));
-
         }
+
+        [Test]
+        public async Task DrinkControllerCreatesNewDrink()
+        {
+                // Arrange
+                var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+                {
+                });
+                var client = factory.CreateClient();
+                var url = "/drink";
+
+                var drink =  new Drink { Id = Guid.NewGuid(), Name = "Test drink" } ;
+                var payload = JsonConvert.SerializeObject(drink);
+                var request = new StringContent(payload, Encoding.UTF8, "application/json");
+
+                // Act
+
+                var response = await client.PostAsync(url, request);
+                var content = await response.Content.ReadAsStringAsync();
+
+                // Assert
+                response.EnsureSuccessStatusCode();
+
+                //Assert.That(content.Equals("value2"));
+        }
+
+        // drink with a non-existant ingredient
+        //test
     }
 }
