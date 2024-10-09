d49467619d3c2a5f02ece3c06aef6bca13cd541f
Jake Atkinson <jake.atkinson@hotmail.com>
2024-10-10 00:41:22 +1100

    drinkController Get tests

--- CocktailStrategistIntegrationTests/DrinkController.test.cs
+++ CocktailStrategistIntegrationTests/DrinkController.test.cs
@@ -2,6 +2,7 @@ using CocktailStrategist.Data;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
+using System.Net;
 using System.Text;
 using System.Transactions;
 
@@ -44,37 +45,51 @@ namespace CocktailStrategistIntegrationTests
             var url = $"/drink/{drink.Id}";
 
             // Act
-
             var response = await client.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
 
             // Assert
             response.EnsureSuccessStatusCode();
             var retrievedDrink = JsonConvert.DeserializeObject<Drink>(content);
+            retrievedDrink.Should().BeEquivalentTo(drink, o => o.ComparingByMembers<Drink>());
+        }
+
+        [Test]
+        public async Task Get_returns404WithIncorrectID()
+        {
+            // Arrange
+            var drink = new Drink { Id = Guid.NewGuid(), Name = "Fake" };
+            var url = $"/drink/{drink.Id}";
+
+            // Act
+            var response = await client.GetAsync(url);
 
             // Assert
-            response.EnsureSuccessStatusCode();
-
-            retrievedDrink.Should().BeEquivalentTo(drink, o => o.ComparingByMembers<Drink>());
-
+            response.Should().HaveStatusCode(HttpStatusCode.NotFound);
         }
 
         [Test]
         public async Task Post_createsNewDrink()
         {
-                // Arrange
+            // Arrange
                 
-                var url = "/drink";
+            var url = "/drink";
+            var drink =  new Drink { Id = Guid.NewGuid(), Name = "Test drink" } ;
+            var payload = JsonConvert.SerializeObject(drink);
+            var request = new StringContent(payload, Encoding.UTF8, "application/json");
 
-                var drink =  new Drink { Id = Guid.NewGuid(), Name = "Test drink" } ;
-                var payload = JsonConvert.SerializeObject(drink);
-                var request = new StringContent(payload, Encoding.UTF8, "application/json");
+            // Act
+            var createResponse = await client.PostAsync(url, request);
 
-                // Act
+            var getUrl = $"/drink/{drink.Id}";
+            var getResponse = await client.GetAsync(getUrl);
+            var content = await getResponse.Content.ReadAsStringAsync();
 
-                var response = await client.PostAsync(url, request);
-                var content = await response.Content.ReadAsStringAsync();
-                
+            // Assert
+            createResponse.EnsureSuccessStatusCode();
+            var retrievedDrink = JsonConvert.DeserializeObject<Drink>(content);
+            retrievedDrink.Should().BeEquivalentTo(drink, o => o.ComparingByMembers<Drink>());
+
         }
 
         // drink with a non-existant ingredient
